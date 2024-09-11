869cc3b8095dc48dba8d87cc3cc43c43146fc59a
Raghavan <raghavanmarch2002@gmail.com>
2024-09-11 18:51:53 +0530

    fix csv parser

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,26 +1,24 @@
 export function csvToJson(text, quoteChar = '"', delimiter = ",") {
     text = text.trim()
     let rows = text.split("\n")
-    let headers = rows[0].split(",")
+
+    // Trim headers to remove any whitespace or carriage return characters
+    let headers = rows[0].split(delimiter).map((header) => header.trim())
 
     const regex = new RegExp(`\\s*(${quoteChar})?(.*?)\\1\\s*(?:${delimiter}|$)`, "gs")
 
     const match = (line) => {
         const matches = [...line.matchAll(regex)].map((m) => m[2])
-        // Ensure matches length matches headers length by padding with null values
         const paddedMatches = Array.from({ length: headers.length }, (_, i) => matches[i] ?? null)
         return paddedMatches
     }
 
-    let lines = text.split("\n")
-    const heads = headers ?? match(lines.shift())
-    lines = lines.slice(1)
+    let lines = text.split("\n").slice(1)
 
     return lines.map((line) => {
         return match(line).reduce((acc, cur, i) => {
-            // replace blank matches with `null`
             const val = cur === null || cur.length <= 0 ? null : Number(cur) || cur
-            const key = heads[i] ?? `{i}`
+            const key = headers[i] ?? `{i}`
             return { ...acc, [key]: val }
         }, {})
     })
